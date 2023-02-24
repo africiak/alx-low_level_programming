15aaef3bf30a55b6c4e5794cf7ac6f01cde5867a
Africia <keruboafricia@gmail.com>
2023-02-24 13:43:46 -0800

    alx

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,13 @@
 /**
  * print_numbers - print numbers
  *
- * Description: prints the numbers, from 0 to 9, followed by a new line
- * Return: Always (0)
  */
 
 void print_numbers(void)
 {
-	char i;
+	int x;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
+	for (x = 0; x < 10; x++)
+		_putchar(x + '0');
 	_putchar('\n');
 }
